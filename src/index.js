08a9d3db7ad5c1355c3c15750e47bdc613094f9d
danilomoita <136601200+danilomoita@users.noreply.github.com>
2024-07-16 11:13:42 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from './pages/home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/video';
+import CadastroCategoria from './pages/cadastro/categoria';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro/video" component={CadastroVideo} exact/>
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact/>
+      <Route component={() => (<div> Página 404</div>)} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
